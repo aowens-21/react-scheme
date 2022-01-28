bc8eb0034762336baa21d6cd4305240dc41373dc
aowens-21 <aowens5189@gmail.com>
2022-01-28 11:20:19 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Repl from './Repl.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <main className="app">
+            <h1>React Scheme</h1>
+            <section className="repl">
+                <Repl />
+            </section>
+        </main>
+    )
 }
-
-export default App;
---
+++ src/Repl.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default class Repl extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            programText: ''
+        }
+
+        this.handleInput = this.handleInput.bind(this);
+    }
+
+    handleInput(event) {
+        this.setState({ programText: event.target.value });
+        console.log(this.state.programText);
+    }
+
+    render() {
+        return (
+            <input 
+                type="text" 
+                className="replInput" 
+                placeholder="Enter Scheme Code Here"
+                value={this.state.programText}
+                onChange={this.handleInput}
+            />
+        )
+    }
+}
