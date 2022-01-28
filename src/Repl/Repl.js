c096292f81c14d65f5b6d43c7222a848bc3c9a63
aowens-21 <aowens5189@gmail.com>
2022-01-28 15:03:45 -0600

    Working on parser

--- src/Repl/Repl.js
+++ src/Repl/Repl.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Instruction, OpType } from "./Language";
+import { Instruction, ExprType } from "./Language";
 
 export default class Repl extends React.Component {
     constructor(props) {
@@ -13,7 +13,7 @@ export default class Repl extends React.Component {
 
     handleInput(event) {
         this.setState({ programText: event.target.value });
-        console.log(this.state.programText);
+        parseProgram(event.target.value);
     }
 
     render() {
@@ -31,5 +31,76 @@ export default class Repl extends React.Component {
 
 export function parseProgram(programText) {
     const trimmedProgram = programText.trim();
-    throw "Not implemented";
+    
+    // find the first non-whitespace char
+    while (trimmedProgram !== '' && 
+           (trimmedProgram[0] === '\n' 
+            || trimmedProgram[0] === ' ' 
+            || trimmedProgram[0] === '\t')) {
+        trimmedProgram = trimmedProgram.substring(1);
+    }
+
+    if (trimmedProgram[0] === '(') {
+        let unparsedSubExprs = [];
+        let currentSubExpr = '';
+        let i = 1;
+        while (i < trimmedProgram.length) {
+            if (trimmedProgram[i] === '(') {
+                const closingParenPos = findMatchingParen(i, trimmedProgram);
+                
+                if (closingParenPos) {
+                    unparsedSubExprs.push(trimmedProgram.substring(i, closingParenPos + 1));
+                    currentSubExpr = '';
+                    i = closingParenPos + 1;
+                } else {
+                    throw new Error('Unbalanced parens!');
+                }
+            } else if (trimmedProgram[i] !== ' ' && trimmedProgram[i] !== '\n' && trimmedProgram[i] !== '\t') {
+                currentSubExpr += trimmedProgram[i];
+                i++;
+            } else {
+                unparsedSubExprs.push(currentSubExpr);
+                currentSubExpr = '';
+                i++;
+            }
+        }
+        
+        unparsedSubExprs.push(currentSubExpr.substring(0, currentSubExpr.length - 1));
+        unparsedSubExprs = unparsedSubExprs.filter((e)=> {
+            return e !== '';
+        });
+        
+        switch (unparsedSubExprs[0]) {
+            case '+':
+                
+        }
+
+    } else {
+        const pAsNumber = parseInt(trimmedProgram);
+
+        if (!isNaN(pAsNumber)) {
+            return {
+                type: ExprType.Num,
+                value: pAsNumber
+            };
+        }
+
+        throw new Error('Invalid program: ' + trimmedProgram);
+    }   
+}
+
+function findMatchingParen(openPos, str) {
+    let extras = 0;
+
+    for (let i = openPos + 1; i < str.length; i++) {
+        if (str[i] === ')' && extras === 0) {
+            return i;
+        } else if (str[i] === ')') {
+            extras--;
+        } else if (str[i] === '(') {
+            extras++;
+        }
+    }
+
+    return false;
 }
--- src/Repl/Repl.test.js
+++ src/Repl/Repl.test.js
@@ -1,14 +1,14 @@
 import { parseProgram } from "./Repl";
-import { OpType } from './Language';
+import { ExprType } from './Language';
 
 test('parse addition', ()=> {
     const p = '     (+ 2 2)  ';
     const parsed = parseProgram(p);
 
-    expect(parsed.type === OpType.Add);
-    expect(parsed.expr1.OpType === OpType.Num);
+    expect(parsed.type === ExprType.Add);
+    expect(parsed.expr1.ExprType === ExprType.Num);
     expect(parsed.expr1.value === 2);
-    expect(parsed.expr2.OpType === OpType.Num);
+    expect(parsed.expr2.ExprType === ExprType.Num);
     expect(parsed.expr2.value === 2);
 });
 
@@ -16,10 +16,10 @@ test('parse subtraction', ()=> {
     const p = '     (- 3 1)  ';
     const parsed = parseProgram(p);
 
-    expect(parsed.type === OpType.Add);
-    expect(parsed.expr1.OpType === OpType.Num);
+    expect(parsed.type === ExprType.Add);
+    expect(parsed.expr1.ExprType === ExprType.Num);
     expect(parsed.expr1.value === 3);
-    expect(parsed.expr2.OpType === OpType.Num);
+    expect(parsed.expr2.ExprType === ExprType.Num);
     expect(parsed.expr2.value === 1);
 });
 
@@ -27,19 +27,25 @@ test('parse nested addition', ()=> {
     const p = '     (+ 2 (+ 1 5))  ';
     const parsed = parseProgram(p);
 
-    expect(parsed.type === OpType.Add);
-    expect(parsed.expr1.OpType === OpType.Num);
+    expect(parsed.type === ExprType.Add);
+    expect(parsed.expr1.ExprType === ExprType.Num);
     expect(parsed.expr1.value === 2);
-    expect(parsed.expr2.OpType === OpType.Add);
+    expect(parsed.expr2.ExprType === ExprType.Add);
 
     const rhs = parsed.expr2;
-    expect(rhs.expr1.type === OpType.Num && rhs.expr1.value === 1);
-    expect(rhs.expr2.type === OpType.Num && rhs.expr2.value === 5);
+    expect(rhs.expr1.type === ExprType.Num && rhs.expr1.value === 1);
+    expect(rhs.expr2.type === ExprType.Num && rhs.expr2.value === 5);
 });
 
 test('parse a single number', ()=> {
     const p = '5';
     const parsed = parseProgram(p);
 
-    expect(parsed.type === OpType.Num && parsed.value === 5);
+    expect(parsed.type === ExprType.Num && parsed.value === 5);
 });
+
+test('throw an error when given an incomplete expression', ()=> {
+    expect(()=> {
+        parseProgram('  (+ ) ');
+    }).toThrow();
+})
